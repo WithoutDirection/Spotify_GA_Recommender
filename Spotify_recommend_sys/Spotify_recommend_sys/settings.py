c08edc4b7d2b33d2a03220e28340fd1bba76edfb
aswe_without <wc980242@gmail.com>
2025-05-07 21:15:18 +0800

    20250507 update deploy website-2

--- Spotify_recommend_sys/Spotify_recommend_sys/settings.py
+++ Spotify_recommend_sys/Spotify_recommend_sys/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-p#0dry&-s2za6&h9!^$)_9r(1&3!j7sl=jnm=jdcwz0$ul-puc'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
@@ -48,6 +48,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'Spotify_recommend_sys.urls'
@@ -74,11 +75,13 @@ WSGI_APPLICATION = 'Spotify_recommend_sys.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+import dj_database_url
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=f'sqlite:///{os.path.join(BASE_DIR, "db.sqlite3")}',
+        conn_max_age=600
+    )
 }
 
 
@@ -129,4 +132,8 @@ ALLOWED_HOSTS = ['Spotify_recommend_sys.onrender.com']
 import os
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
+
+# Add for production
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
